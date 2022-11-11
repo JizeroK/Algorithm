7ea4c96d8caddad5974fe818ea280c035166c066
KimJY <117333047+JizeroK@users.noreply.github.com>
2022-11-11 13:58:55 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -2,15 +2,16 @@ class Solution {
     public boolean solution(int x) {
         boolean answer = true;
         int sum=0;
-        
-        String tmp=""+x;
-        String[] str=tmp.split("");
-        for(String s: str){
-            sum+=Integer.parseInt(s);
+        int tmpx=x;
+
+        while(tmpx>0){
+            sum+=tmpx%10;
+            tmpx=tmpx/10;
         }
-        
-        if(x%sum!=0) answer=false; 
-        
+
+        if(x%sum!=0)
+            answer=false;
+
         return answer;
     }
 }
