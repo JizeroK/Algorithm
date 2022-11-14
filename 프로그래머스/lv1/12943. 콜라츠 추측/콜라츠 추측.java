8decddc8c7b1c94e4ede5cf1c2cfec3d83d55562
KimJY <117333047+JizeroK@users.noreply.github.com>
2022-11-14 18:18:03 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,24 +1,20 @@
+
 class Solution {
-    public int solution(int num) {
+    public int solution(long num) {
         int answer = 0;
         
-        while(true) {        //1이면 반복문 종료
-            if(num==1) break;
-            if(num%2==0) {
-                num=num/2;
-            }
-            else if(num%2==1) {  
-                num=(num*3)+1; 
+        while(num!=1){
+            if(num%2==0){
+                num/=2;
+            }else{
+                num=num*3+1;
             }
             
             answer++;
-            
-            if(answer==500) { 
-                answer=-1;  
-                break;
+            if(answer==500){
+                answer=-1; break;
             }
         }
-        
         return answer;
     }
 }
