1eb8771ede5a4a7b5fc29a56650a933a73ce8520
KimJY <117333047+JizeroK@users.noreply.github.com>
2022-11-16 23:08:47 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.57 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -4,7 +4,7 @@ class Solution {
         String[] str={"수", "박"};
         
         for(int i=0; i<n; i++){
-            if(i==0||i%2==0) answer+=str[0];
+            if(i%2==0) answer+=str[0];
             else answer+=str[1];
         }
         return answer;
