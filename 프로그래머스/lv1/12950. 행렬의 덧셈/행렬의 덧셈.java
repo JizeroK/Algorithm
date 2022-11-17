cc94c69d6efc8214e620088dc37eb249a67a8876
KimJY <117333047+JizeroK@users.noreply.github.com>
2022-11-17 23:23:02 +0900

    [level 1] Title: 행렬의 덧셈, Time: 3.68 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,19 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = {};
         
-        int i=0;
-        int j=0;
-        for(i=0; i<arr1.length; i++){
-            for(j=0; j<arr1[i].length; j++){
-                
-            }
-        }
-        answer = new int[i][j];
+        answer=Arrays.copyOf(arr1, arr1.length);
         
-        for(int x=0; x<arr1.length; x++){
-            for(int y=0; y<arr1[x].length; y++){
-                answer[x][y]=arr1[x][y]+arr2[x][y];
+        for(int i=0; i<arr1.length; i++){
+            for(int j=0; j<arr1[i].length; j++){
+                answer[i][j]=arr1[i][j]+arr2[i][j];
             }
         }
         
