939c94fbba1e38c18ced0662639580f12cfbb876
KimJY <117333047+JizeroK@users.noreply.github.com>
2022-11-18 23:43:21 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.96 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -12,8 +12,12 @@ public class Solution {
             list.add(arr[i]);
         }
         
-        answer=list.stream().mapToInt(Integer::intValue).toArray();
-
+        //answer=list.stream().mapToInt(Integer::intValue).toArray();
+        answer=new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i]=list.get(i);
+        }
+        
         return answer;
     }
 }
