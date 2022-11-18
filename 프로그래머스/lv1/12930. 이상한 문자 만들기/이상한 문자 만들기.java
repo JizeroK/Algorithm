aa3331643a0722ca9fcb961a04a5f64d42e4b42f
KimJY <117333047+JizeroK@users.noreply.github.com>
2022-11-19 01:04:37 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 8.21 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        boolean check=true;
+        StringBuilder builder=new StringBuilder(s);
+        char c;
+        
+        for(int i=0; i<s.length(); i++){
+            c=s.charAt(i);
+            if(c==' ') check=false;
+            if(check){  //check==true 일때
+                builder.setCharAt(i, Character.toUpperCase(c));
+                check=false;
+            }
+            else{
+                builder.setCharAt(i, Character.toLowerCase(c));
+                check=true;
+            }
+        }
+        
+        answer=builder+"";
+        
+        return answer;
+    }
+}
+
+/*
+        String[] str = s.split(" ");
+        int count=0;
+        StringBuilder builder=null;
+        
+        while(count<str.length){
+            builder = new StringBuilder(str[count]);
+            for(int i=0; i<str[count].length(); i++){
+                char c=str[count].charAt(i);
+                if(i%2==0&&Character.isLowerCase(c)) {
+                    builder.setCharAt(i, Character.toUpperCase(c));
+                }
+                else if(i%2!=0&&Character.isUpperCase(c)){
+                    builder.setCharAt(i, Character.toLowerCase(c));
+                }
+            }
+            answer+=builder+" ";
+            count++;
+        }
+        
+        answer=answer.trim();
+*/
