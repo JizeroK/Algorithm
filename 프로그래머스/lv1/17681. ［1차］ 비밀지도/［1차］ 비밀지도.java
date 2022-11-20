a243901feeb12e0a15c7d6e3d359ce19c2e247c1
KimJY <117333047+JizeroK@users.noreply.github.com>
2022-11-20 22:18:05 +0900

    [level 1] Title: [1차] 비밀지도, Time: 6.30 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -3,15 +3,13 @@ class Solution {
         String[] answer = {};
         String result;
         
+        
         answer=new String[arr1.length];
         for(int i=0; i<arr1.length; i++){
-            //System.out.println(Integer.toBinaryString(arr1[i]|arr2[i]));
-        
             result = String.format("%" + arr1.length + "s", Integer.toBinaryString(arr1[i]|arr2[i]));
-            //System.out.println(result);
-            
             result=result.replace("1", "#");
             result=result.replace("0", " ");
+            
             answer[i]=result;
         }
         
