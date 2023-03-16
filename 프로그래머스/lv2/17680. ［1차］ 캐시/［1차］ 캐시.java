89feb72940658c33e6b230441be729fcf862a6af
KimJY <117333047+JizeroK@users.noreply.github.com>
2023-03-17 01:03:29 +0900

    [level 2] Title: [1차] 캐시, Time: 2.61 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,39 +1,35 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
         String[] cache = new String[cacheSize];
-        int j=0;
-        int num=0;
-        
-        for(int i=0; i<cities.length; i++){
-            if(cacheSize==0){   //캐시 사이즈가 0이면
+
+        Loop1 : for(int i=0; i<cities.length; i++){
+            if(cacheSize==0){ 
                 answer=cities.length*5;
                 break;
             }
-            
-            for(j=0; j<cacheSize; j++){  
-                if(cities[i].equalsIgnoreCase(cache[j])){ //같은게 있으면
+
+            for(int j=0; j<cacheSize; j++){  
+                if(cities[i].equalsIgnoreCase(cache[j])){ 
                     answer+=1;
-                    num = j;
-                    break;
+                    cache[j]=null;
+                    for(int k=j; k>=1; k--){
+                        cache[k]=cache[k-1];
+                    }
+                    cache[0]=cities[i];
+                    continue Loop1;
                 }
             }
-            
-            if(j==cacheSize){ //같은게 없으면
-                answer+=5;
-                num=j-1;
-            }
-                
-            cache[num]=null;
-            for(int k=num; k>=1; k--){
+
+            answer+=5;
+            cache[cacheSize-1]=null;
+            for(int k=cacheSize-1; k>=1; k--){
                 cache[k]=cache[k-1];
             }
             cache[0]=cities[i];
             
         }
-        
+
         return answer;
     }
 }
